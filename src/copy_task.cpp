d69dfe9a76fc8c0eda89bad17e874af6b9e91802
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-08-03 12:04:45 -0400

    Add bits for copy_task

---
+++ src/copy_task.cpp
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2026 Dimitry Ishenko
+// Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
+//
+// Distributed under the GNU GPL license. See the LICENSE.md file for details.
+
+////////////////////////////////////////////////////////////////////////////////
+#include "state.hpp"
+
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+////////////////////////////////////////////////////////////////////////////////
+void copy_task(state& state, const fs::path& source, const fs::path& target)
+{
+    // TODO
+}
