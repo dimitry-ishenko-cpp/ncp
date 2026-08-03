f0f72fe73b0e33e0ae602db4ecf6bc312ce20e35
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-08-03 12:33:39 -0400

    walk_task: implement basic walk

--- src/walk_task.cpp
+++ src/walk_task.cpp
@@ -9,9 +9,84 @@
 #include "state.hpp"
 
 #include <asio.hpp>
+#include <filesystem>
+#include <generator>
+#include <ranges>
+
+namespace fs = std::filesystem;
+
+void copy_task(state& state, const fs::path&, const fs::path&);
+
+////////////////////////////////////////////////////////////////////////////////
+inline void throw_filesystem_error(std::errc cond)
+{
+    throw fs::filesystem_error{"walk_task", std::make_error_code(cond)};
+}
+inline void throw_filesystem_error(std::errc cond, const fs::path& path)
+{
+    throw fs::filesystem_error{"walk_task", path, std::make_error_code(cond)};
+}
+
+std::generator<fs::directory_entry> walk(const fs::path& dir)
+{
+    for (auto& entry : fs::directory_iterator(dir))
+    {
+        co_yield entry;
+        if (entry.is_directory()) co_yield std::ranges::elements_of( walk(entry.path()) );
+    }
+}
+
+void post_copy(state& state, asio::thread_pool& pool, fs::path source, fs::path target)
+{
+    asio::post(pool, [&state, s = std::move(source), t = std::move(target)]{ copy_task(state, s, t); });
+}
+
+void walk_dir(state& state, asio::thread_pool& pool, const fs::path& source, const fs::path& target)
+{
+    fs::create_directory(target);
+
+    for (auto&& entry : walk(source))
+    {
+        auto target_path = target / fs::relative(entry.path(), source);
+
+        if (entry.is_directory()) fs::create_directory(target_path);
+        else post_copy(state, pool, entry.path(), target_path);
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 void walk_task(state& state, const options& options, asio::thread_pool& pool)
 {
-    // TODO
+    if (fs::is_directory(options.target))
+    {
+        for (auto&& source : options.sources)
+        {
+            if (!fs::exists(source)) throw_filesystem_error(std::errc::no_such_file_or_directory, source);
+
+            if (fs::is_directory(source))
+            {
+                auto trailing_slash = source.native().back() == fs::path::preferred_separator;
+                auto target = trailing_slash ? options.target : options.target / source.filename();
+
+                walk_dir(state, pool, source, target);
+            }
+            else
+            {
+                auto target = options.target / source.filename();
+                post_copy(state, pool, source, target);
+            }
+        }
+    }
+    else
+    {
+        if (options.sources.size() > 1) throw_filesystem_error(std::errc::not_a_directory, options.target);
+
+        auto&& source = options.sources.front();
+        auto&& target = options.target;
+
+        if (!fs::exists(source)) throw_filesystem_error(std::errc::no_such_file_or_directory, source);
+
+        if (fs::is_directory(source)) walk_dir(state, pool, source, target);
+        else post_copy(state, pool, source, target);
+    }
 }
