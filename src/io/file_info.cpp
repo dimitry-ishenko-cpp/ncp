2b6b17cf52a3eabbc8ebb10820aa7b9fc93a65e5
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-08-06 23:24:07 -0400

    Add file_info bits

---
+++ src/io/file_info.cpp
@@ -0,0 +1,68 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2026 Dimitry Ishenko
+// Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
+//
+// Distributed under the GNU GPL license. See the LICENSE.md file for details.
+
+////////////////////////////////////////////////////////////////////////////////
+#include "file_info.hpp"
+
+#include <cerrno>
+#include <sys/stat.h>
+
+////////////////////////////////////////////////////////////////////////////////
+namespace io
+{
+
+file_info::file_info(io::path path, bool follow_symlinks, error_code& ec) noexcept :
+    path_{std::move(path)}
+{
+    ec.clear();
+
+    struct stat st{};
+    auto pfn = follow_symlinks ? ::stat : ::lstat;
+
+    if (pfn(path_.c_str(), &st))
+    {
+        if (errno == ENOENT || errno == ENOTDIR)
+            type_ = file_type::not_found;
+        else ec = error_code{ errno, std::generic_category() };
+    }
+    else
+    {
+        if (S_ISREG(st.st_mode)) type_ = file_type::regular;
+        else if (S_ISDIR (st.st_mode)) type_ = file_type::directory;
+        else if (S_ISLNK (st.st_mode)) type_ = file_type::symlink;
+        else if (S_ISBLK (st.st_mode)) type_ = file_type::block;
+        else if (S_ISCHR (st.st_mode)) type_ = file_type::character;
+        else if (S_ISFIFO(st.st_mode)) type_ = file_type::fifo;
+        else if (S_ISSOCK(st.st_mode)) type_ = file_type::socket;
+        else type_ = file_type::unknown;
+
+        size_  = static_cast<io::file_size>(st.st_size);
+        perms_ = static_cast<io::perms>(st.st_mode & 07777);
+        uid_   = static_cast<io::uid>(st.st_uid);
+        gid_   = static_cast<io::gid>(st.st_gid);
+        hardlink_count_ = static_cast<io::hardlink_count>(st.st_nlink);
+    }
+}
+
+std::expected<file_info, error_code> file_info::get(io::path path) noexcept
+{
+    error_code ec;
+    file_info info{std::move(path), ec};
+
+    if (ec) return std::unexpected(ec);
+    else return info;
+}
+
+std::expected<file_info, error_code> file_info::get(io::path path, follow_symlinks_t) noexcept
+{
+    error_code ec;
+    file_info info{std::move(path), follow_symlinks, ec};
+
+    if (ec) return std::unexpected(ec);
+    else return info;
+}
+
+}
---
+++ src/io/file_info.hpp
@@ -0,0 +1,87 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2026 Dimitry Ishenko
+// Contact: dimitry (dot) ishenko (at) (gee) mail (dot) com
+//
+// Distributed under the GNU GPL license. See the LICENSE.md file for details.
+
+////////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#include <cstdint>
+#include <expected>
+#include <filesystem>
+#include <system_error>
+
+////////////////////////////////////////////////////////////////////////////////
+namespace io
+{
+
+using std::filesystem::file_type;
+using std::filesystem::path;
+using std::filesystem::perms;
+
+using file_size = std::uintmax_t;
+using hardlink_count = std::uintmax_t;
+
+using uid = std::uint32_t;
+using gid = std::uint32_t;
+
+using std::error_code;
+
+struct follow_symlinks_t { explicit follow_symlinks_t() = default; };
+inline constexpr follow_symlinks_t follow_symlinks{};
+
+////////////////////////////////////////////////////////////////////////////////
+class file_info
+{
+    io::path path_;
+
+    io::file_type type_ = file_type::none;
+    io::file_size size_;
+    io::perms perms_;
+    io::uid uid_;
+    io::gid gid_;
+    io::hardlink_count hardlink_count_;
+
+public:
+    file_info() noexcept = default;
+    file_info(io::path path, error_code& ec) noexcept : file_info{std::move(path), false, ec} { }
+    file_info(io::path path, follow_symlinks_t, error_code& ec) noexcept : file_info{std::move(path), true, ec} { }
+
+    static std::expected<file_info, error_code> get(io::path) noexcept;
+    static std::expected<file_info, error_code> get(io::path, follow_symlinks_t) noexcept;
+
+    auto& path() const noexcept { return  path_; }
+
+    auto  type() const noexcept { return  type_; }
+    auto  size() const noexcept { return  size_; }
+    auto perms() const noexcept { return perms_; }
+    auto   uid() const noexcept { return   uid_; }
+    auto   gid() const noexcept { return   gid_; }
+    auto hardlink_count() const noexcept { return hardlink_count_; }
+
+    bool exists() const noexcept { return type_ != file_type::none && type_ != file_type::not_found; }
+    explicit operator bool() const noexcept { return exists(); }
+
+    bool is_regular_file() const noexcept { return type_ == file_type::regular;   }
+    bool is_directory   () const noexcept { return type_ == file_type::directory; }
+    bool is_symlink     () const noexcept { return type_ == file_type::symlink;   }
+    bool is_block_device() const noexcept { return type_ == file_type::block;     }
+    bool is_char_device () const noexcept { return type_ == file_type::character; }
+    bool is_fifo        () const noexcept { return type_ == file_type::fifo;      }
+    bool is_socket      () const noexcept { return type_ == file_type::socket;    }
+
+    bool is_standard    () const noexcept { return is_regular_file() || is_directory()   || is_symlink(); }
+    bool is_special     () const noexcept { return is_block_device() || is_char_device() || is_fifo() || is_socket(); }
+
+    std::expected<file_info, error_code> target() const
+    {
+        if (!is_symlink()) return *this;
+        else return file_info::get(path_, follow_symlinks);
+    }
+
+private:
+    file_info(io::path, bool follow_symlinks, error_code&) noexcept;
+};
+
+}
