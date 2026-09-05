f02b49d9b3c5b1a1f318f20cf65d46888d14a738
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2026-09-04 23:16:12 -0400

    io: allow desc with negative fd (eg AT_FDCWD)

--- src/io/desc.hpp
+++ src/io/desc.hpp
@@ -24,10 +24,10 @@ class desc
 public:
     constexpr desc() noexcept = default;
     constexpr explicit desc(int fd) noexcept : fd_{fd} { }
-    ~desc() { if (fd_ != -1) ::close(fd_); }
+    constexpr ~desc() { if (fd_ >= 0) ::close(fd_); }
 
     desc(const desc& rhs) noexcept :
-        fd_{rhs.fd_ != none ? ::fcntl(rhs.fd_, F_DUPFD_CLOEXEC, 0) : none}
+        fd_{rhs.fd_ >= 0 ? ::fcntl(rhs.fd_, F_DUPFD_CLOEXEC, 0) : rhs.fd_}
     { }
     constexpr desc(desc&& rhs) noexcept : fd_{std::exchange(rhs.fd_, none)} { }
     constexpr auto& operator=(desc rhs) noexcept { std::swap(fd_, rhs.fd_); return *this; }
